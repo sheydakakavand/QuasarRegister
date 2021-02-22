ce6b8bf434701a56e8a28154d169999b054f6523
sheyda <65152757+sheydakakavand@users.noreply.github.com>
2021-02-22 09:23:19 +0330

    Add files via upload

---
+++ search.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-layout view="hHh Lpr fFf"> <!-- Be sure to play with the Layout demo on docs -->
+
+    <!-- (Optional) The Header -->
+    <q-header class="row">
+      <q-toolbar class="col"  >
+        <q-toolbar-title>
+        website Name
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs>
+
+      </q-tabs>
+
+    </q-header>
+
+    <q-page-container class="absolute-right" >
+      <!-- This is where pages get injected -->
+      <router-view  />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  // name: 'LayoutName',
+
+  data () {
+    return {
+      leftDrawer: true
+    }
+  }
+}
+</script>
